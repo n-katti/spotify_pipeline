26b951a104dbbe3842f8d65f798d8d1dcb9c9f6e
Nikhil <nikhilkatti@outlook.com>
2023-05-15 22:40:51 -0400

    Added requirements file:

---
+++ modules/spotify/spotify_api.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import spotipy as sp
+import sqlalchemy as sql
+import numpy as n
+import psycopg2 as ps
+
+print(pd.__version__)
+print(sp.__version__)
+print(sql.__version__)
+print(n.__version__)
+print(ps.__version__)
